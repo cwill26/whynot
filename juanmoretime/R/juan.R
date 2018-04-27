65a30e741bfcda6beb00914cabaef62f3ff3179b
cwill26 <clnwllms190@gmail.com>
2018-04-27 18:35:19 -0500

    Final update to documentation

--- juanmoretime/R/juan.R
+++ juanmoretime/R/juan.R
@@ -1,3 +1,11 @@
+#' @title An homage to our favorite student, Juan
+#' 
+#' @description Allows for random sampling of letters from the built-in letters object
+#' 
+#' @param n the number of letters you want to draw
+#' @return a vector of n randomly selected letters, with replacement
+#' 
+#' @export
 juan <-
 function(n = 1){
   return(sample(letters, n))
